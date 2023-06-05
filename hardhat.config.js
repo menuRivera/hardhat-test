3325da02a4077ed10b5dcff14f52a342f1a3cb00
Manuel Rivera <menu.rivera@gmail.com>
2023-06-04 20:07:14 -0600

    [ADD] -> hardhat.config.js configuration for deploying to real networks comment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require('@nomicfoundation/hardhat-toolbox')
 
+/*
+// Deploying to a real network
+const INFURA_API_KEY = '<key>'
+const SEPOLIA_PRIVATE_KEY = '<key>'
+
+module.exports = {
+	solidity: "0.8.18",
+	networks: {
+		sepolia: {
+			url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+			accounts: [SEPOLIA_PRIVATE_KEY]
+		}
+	}
+};
+
+// running the command
+// npx hardhat run scripts/deploy.js --network sepolia
+*/
+
 module.exports = {
   solidity: "0.8.18",
 };
+
+
